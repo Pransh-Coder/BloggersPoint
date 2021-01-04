37bada2fb98988d9fb6c02fccfd18d496a55599f
Pransh-Coder <rastogipransh4@gmail.com>
2021-01-05 00:33:43 +0530

    Added the blog detail fragment

---
+++ app/src/main/java/com/customer/bloggerspoint/homeStructure/BlogsDetailFragment.java
@@ -0,0 +1,39 @@
+package com.customer.bloggerspoint.homeStructure;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.customer.bloggerspoint.R;
+
+
+public class BlogsDetailFragment extends Fragment {
+
+    TextView tv_title,tv_description,tv_authorName;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blogs_detail, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        tv_title = view.findViewById(R.id.title);
+        tv_description = view.findViewById(R.id.description);
+        tv_authorName = view.findViewById(R.id.authorName);
+
+
+
+    }
+}
